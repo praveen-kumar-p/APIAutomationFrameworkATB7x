0ecd1a2fc8ca808b635ae9a488e02967a4cebbc5
HP <praveenkumarpiyli018@gmail.com>
2024-09-29 15:02:46 +0530

    Frame Work simple code

---
+++ src/test/java/org/example/tests/CRUD/TestCreatebookingGET.java
@@ -0,0 +1,16 @@
+package org.example.tests.CRUD;
+
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCreatebookingGET {
+
+
+    @Description("Verify that post request is working fine")
+    @Test
+    public void testcreateGET01 (){
+        Assert.assertEquals(true, true);
+
+    }
+}
